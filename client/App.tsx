b667864d635cc6f1eb976b3be4f8c0eb24f2eb1d
kgadzinowski <kgadzinowski@gmail.com>
2022-09-04 11:58:09 +0200

    Removed a toaster

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,9 @@
 import { AppClient, ClientPage } from "../node_modules/rapdv-lib/dist/client/main.js"
 import "../node_modules/rapdv-lib/dist/client/main.css"
 import { PageEditPost } from "./PageEditPost"
-import { PageLogIn } from "./PageLogIn"
 import "./styles/style.scss"
 
 const pages: Array<ClientPage> = []
-pages.push(new PageLogIn())
 pages.push(new PageEditPost())
 
 const app = new AppClient()
