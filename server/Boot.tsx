b91d906723af2d39bc1393b8ff362d7411d84dea
Konrad Gadzinowski <kgadzinowski@gmail.com>
2023-10-27 14:08:00 +0200

    Update booting

--- server/Boot.tsx
+++ server/Boot.tsx
@@ -1,6 +1,8 @@
 import { Server } from "../submodules/rapdv/server/server/Server"
-import { App } from "./App"
 
-const app = new App()
-const server = new Server(app)
+const server = new Server(() => {
+  const App = require("./App").App
+  const app = new App()
+  return app
+})
 server.start()
