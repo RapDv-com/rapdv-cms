0a1170d40ad46419d71237d98864a0d6d4605927
Konrad Gadzinowski <kgadzinowski@gmail.com>
2023-11-03 13:04:01 +0100

    Moved Boot to library

--- server/Boot.tsx
+++
@@ -1,8 +0,0 @@
-import { Server } from "../submodules/rapdv/server/server/Server"
-
-const server = new Server(() => {
-  const App = require("./App").App
-  const app = new App()
-  return app
-})
-server.start()
