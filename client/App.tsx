230e700ba5d3c22dea2ae65b5f43bb95bd15381c
Konrad G <kgadzinowski@gmail.com>
2024-11-25 13:10:23 +0100

    Added Cloudflare Turnslide

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,12 @@
 import { AppClient } from "../submodules/rapdv/client/app/AppClient"
 import { ClientPage } from "../submodules/rapdv/client/elements/PagesCtrl"
 import { PageEditPost } from "./pages/PageEditPost"
+import { PageLogin } from "./pages/PageLogin"
 import "./styles/style.scss"
 
 const pages: Array<ClientPage> = []
 pages.push(new PageEditPost())
+pages.push(new PageLogin())
 
 const app = new AppClient()
 app.start(pages)
